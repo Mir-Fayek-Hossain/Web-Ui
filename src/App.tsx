861fac4e3c90144ff754d41bd1cdd5d355ae72d4
Mir-Fayek-Hossain <mirfayekhossain@gmail.com>
2023-01-26 11:04:47 +0600

    Begginer RTK counter Boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,15 @@
 import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import logo from "./logo.svg"
+import { useAppDispatch, useAppSelector } from "./app/hooks"
+import { decrement, increment } from "./features/counter/counterSlice"
 
 function App() {
+  const counter = useAppSelector((state) => state?.counter?.value)
+  const dispatch = useAppDispatch()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <p>{counter}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
